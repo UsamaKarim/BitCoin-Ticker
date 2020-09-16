553dbff068e57930d2faa6ca38490fc5a6351406
Usama Karim <sonuosama@gmail.com>
2020-09-16 11:46:29 +0500

    Challenge

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const apiKey = 'B386BF07-13E0-42BA-8900-874B024B2ED9';
+const apiKey2 = '76E5FE3D-B712-499E-8149-594480F32648';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +35,16 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<dynamic> coinAPI(String selectACurrency) async {
+    for (String coinList in cryptoList) {}
+    http.Response response = await http.get(
+        'https://rest.coinapi.io/v1/exchangerate/BTC/$selectACurrency?apikey=$apiKey');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      print(response.statusCode);
+      throw 'Your reached max API Calls for today';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io' show Platform;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'crypto_card.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,93 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String coin = 'BTC';
+  String currency = 'USD';
+  String rates = '12345';
+//initialization of Currency to show in DropDownMenuList
+  String selectedCurreny = 'USD';
+  //Initializing a String to put in URL selected by person
+  String selectedByPerson;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI(selectedCurreny);
+  }
+
+  //Android DropdownMenuItem List
+  DropdownButton<String> dropDownMenuItemList() {
+    List<DropdownMenuItem<String>> dropDownMenuList = [];
+    for (String currency in currenciesList) {
+      dropDownMenuList.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+    return DropdownButton<String>(
+      value: selectedCurreny,
+      items: dropDownMenuList,
+      onChanged: (value) {
+        setState(() {
+          selectedCurreny = value;
+        });
+        updateUI(selectedCurreny);
+        return selectedCurreny;
+      },
+    );
+  }
+
+//iOS CupertinoPicker List
+  NotificationListener cupertinoPickerList() {
+    List<Text> textWidgetList = [];
+    for (String curreny in currenciesList) {
+      textWidgetList.add(
+        Text(
+          curreny,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      );
+    }
+    return NotificationListener<ScrollNotification>(
+      onNotification: (scrollNotification) {
+        if (scrollNotification is ScrollEndNotification) {
+          //Will only update when user has stopped scrolling in picker.
+
+          return true;
+        } else {
+          return false;
+        }
+      },
+      child: CupertinoPicker(
+        itemExtent: 30,
+        scrollController: FixedExtentScrollController(initialItem: 19),
+        onSelectedItemChanged: (selectedIndex) {
+          selectedCurreny = currenciesList[selectedIndex];
+          updateUI(selectedCurreny);
+          print(selectedCurreny);
+        },
+        children: textWidgetList,
+      ),
+    );
+  }
+
+  Future updateUI(String selectACurrency) async {
+    try {
+      var coinAPIData = await CoinData().coinAPI(selectedCurreny);
+      setState(() {
+        coin = coinAPIData['asset_id_base'];
+        currency = coinAPIData['asset_id_quote'];
+        rates = coinAPIData['rate'].toStringAsFixed(2);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +107,20 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CryptoCard(coin: coin, rates: rates, currency: currency),
+              CryptoCard(coin: coin, rates: rates, currency: currency),
+              CryptoCard(coin: coin, rates: rates, currency: currency),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: cupertinoPickerList(),
           ),
         ],
       ),
